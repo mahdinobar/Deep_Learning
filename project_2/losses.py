bf78ce06ee6723329dd47f8b2d883aaa67337ec0
Mahdi <44223239+mahdinobar@users.noreply.github.com>
2019-03-31 18:14:17 +0200

    Add files via upload
    CrossEntropy Loss and Initialization added by Yamin and Mahdi

--- project_2/losses.py
+++ project_2/losses.py
@@ -26,3 +26,38 @@ class LossMSE(Module):
         error = self.x - self.target
 
         return 2 * error / (self.target.size(0) * self.target.size(1))
+
+
+class CrossEntropy_Loss(Module):
+   """
+   A class for cross entropy loss
+   """
+   def forward(self,x,target):
+       """
+       Forward pass of the cross entropy loss
+
+       Parameters:
+           x:  main input
+           target: main output
+       Returns:
+           Cross Entropy Loss
+       """
+       self.x=x
+       self.target=target
+       self.N=x.size()[0]
+       exp_fyn=x.gather(1,target.view(-1)).t().exp().squeeze()
+       sigma_exp_fk=sum(x.exp())
+       Loss=(-1./self.N)*(sum((exp_fyn/sigma_exp_fk).log()))
+
+       return Loss
+   def backward(self):
+       """
+       Backward pass of the cross entropy loss
+       Parameters:
+                  -
+       Returns:
+                  Gradient of the cross entropy loss with respect to input
+       """
+
+       # dL_dx=-(1./self.N)*
+
