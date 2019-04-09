7286f020383fdb68ba9b4c1f456e2a9a460b2161
AGUET Clementine <cat@csem.ch>
2019-04-09 08:53:09 +0200

    test file modification

--- project_2/test.py
+++ project_2/test.py
@@ -5,48 +5,17 @@ from losses import LossMSE
 from optimization import SGD
 
 
-# --------------------------------------------------------------------------------------------------
-# DATASET
-# --------------------------------------------------------------------------------------------------
-# Generate datapoints
-nb_samples = 1000
-
-train_input, train_label = generate_data(nb_samples)
-train_label = convert_to_one_hot_labels(train_label)
-
-test_input, test_label = generate_data(nb_samples)
-test_label = convert_to_one_hot_labels(test_label)
-
-print('Training data dimension: ', train_input.size())
-print('Training labels dimension: ', train_label.size())
-
-# Normalize data
-train_input = normalization(train_input)
-test_input = normalization(test_input)
-
-
-# --------------------------------------------------------------------------------------------------
-# MODEL
-# --------------------------------------------------------------------------------------------------
-input_dim = 2
-output_dim = 2
-hidden_dim = 25
-
-model = Sequential(
-        Linear(input_dim, hidden_dim),
-        Tanh(),
-        Linear(hidden_dim, hidden_dim),
-        Tanh(),
-        Linear(hidden_dim, hidden_dim),
-        Tanh(),
-        Linear(hidden_dim, output_dim)
-)
-
-
-# --------------------------------------------------------------------------------------------------
-# TRAINING
-# --------------------------------------------------------------------------------------------------
 def training(m, inputs, targets, batch_size, nb_epochs, lr):
+    """
+    Training function
+    :param m: model
+    :param inputs: input data
+    :param targets:
+    :param batch_size:
+    :param nb_epochs:
+    :param lr: learning rate
+    :return:
+    """
     criterion = LossMSE()
     optimizer = SGD(m, lr, momentum=0.9)
 
@@ -62,14 +31,20 @@ def training(m, inputs, targets, batch_size, nb_epochs, lr):
 
 
 def compute_error(m, inputs, targets, batch_size):
+    """
+    Compute number of errors -> model accuracy
+    :param m:
+    :param inputs:
+    :param targets:
+    :param batch_size:
+    :return:
+    """
     nb_errors = 0
 
-    # over batches
     for batch in range(0, inputs.size(0), batch_size):
         output = m.forward(inputs.narrow(0, batch, batch_size))
         _, prediction = output.max(1)
 
-        # over sample
         for i in range(batch_size):
             if targets[batch + i, prediction[i]] < 0:
                 nb_errors += 1
@@ -77,15 +52,64 @@ def compute_error(m, inputs, targets, batch_size):
     return nb_errors
 
 
-# PARAMETERS
-number_epochs = 250
-learning_rate = 1e-2
-mini_batch_size = 100
+def main():
+    """ Main function """
+    # --------------------------------------------------------------------------------------------------
+    # PARAMETERS
+    # --------------------------------------------------------------------------------------------------
+    nb_samples = 1000
 
-training(model, train_input, train_label, batch_size=mini_batch_size, nb_epochs=number_epochs, lr=learning_rate)
+    # Model
+    input_dim = 2
+    output_dim = 2
+    hidden_dim = 25
 
-# ERROR
-train_error = compute_error(model, train_input, train_label, mini_batch_size) / train_input.size(0) * 100
-test_error = compute_error(model, test_input, test_label,  mini_batch_size) / test_input.size(0) * 100
+    # Training
+    number_epochs = 250
+    learning_rate = 1e-2
+    mini_batch_size = 100
 
-print('Train error {:.02f}% \n Test error {:.02f}%'.format(train_error, test_error))
+    # --------------------------------------------------------------------------------------------------
+    # DATASET
+    # --------------------------------------------------------------------------------------------------
+    # Generate data
+    train_input, train_label = generate_data(nb_samples)
+    train_label = convert_to_one_hot_labels(train_label)
+
+    test_input, test_label = generate_data(nb_samples)
+    test_label = convert_to_one_hot_labels(test_label)
+
+    print('Training data dimension: ', train_input.size())
+    print('Training labels dimension: ', train_label.size())
+
+    # Normalize data
+    train_input = normalization(train_input)
+    test_input = normalization(test_input)
+
+    # --------------------------------------------------------------------------------------------------
+    # MODEL
+    # --------------------------------------------------------------------------------------------------
+    model = Sequential(
+        Linear(input_dim, hidden_dim),
+        Tanh(),
+        Linear(hidden_dim, hidden_dim),
+        Tanh(),
+        Linear(hidden_dim, hidden_dim),
+        Tanh(),
+        Linear(hidden_dim, output_dim)
+    )
+
+    # --------------------------------------------------------------------------------------------------
+    # TRAINING
+    # --------------------------------------------------------------------------------------------------
+    training(model, train_input, train_label, batch_size=mini_batch_size, nb_epochs=number_epochs, lr=learning_rate)
+
+    # ERROR
+    train_error = compute_error(model, train_input, train_label, mini_batch_size) / train_input.size(0) * 100
+    test_error = compute_error(model, test_input, test_label,  mini_batch_size) / test_input.size(0) * 100
+
+    print('Train error {:.02f}% \nTest error {:.02f}%'.format(train_error, test_error))
+
+
+if __name__ == '__main__':
+    main()
