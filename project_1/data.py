4413996b811f8a03ca0f0df2f7783b08d9813d8b
ysepehri <47752486+ysepehri@users.noreply.github.com>
2019-04-26 07:35:21 +0200

    Add files via upload
    A first try for the nn using PyTorch without the help of preprocessing and auxiliary loss, (and with the weight sharing?), Yamin and Mahdi

---
+++ project_1/data.py
@@ -0,0 +1,28 @@
+import torch
+import math
+
+
+def generate_data(nb_samples):
+    """ Generate dataset """
+    # samples uniformly distributed between [0, 1]
+    inputs = torch.empty(nb_samples, 2).uniform_(0, 1)
+
+    # Label = 0 -> outside disk of radius 1/sqrt(2*pi)
+    # Label = 1 -> inside disk
+    targets = (inputs - 0.5).pow(2).sum(1).sub(1 / (2 * math.pi)).sign().mul(-1).add(1).div(2).long()
+
+    return inputs, targets
+
+
+def normalization(data):
+    """ Normalization by mean and standard deviation """
+    mean_value = data.mean(dim=0)
+    std_value = data.std(dim=0)
+    return data.sub_(mean_value).div_(std_value)
+
+
+def convert_to_one_hot_labels(inputs):
+    """ Convert target tensor to one hot vector representation """
+    tmp = torch.FloatTensor(inputs.size(0), inputs.max() + 1).fill_(0)
+    tmp.scatter_(1, inputs.view(-1, 1), 1.0)
+    return tmp
