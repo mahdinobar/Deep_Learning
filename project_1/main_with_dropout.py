ff08b412dee72ce2b4dd75734b8a436967fade4b
Mahdi <44223239+mahdinobar@users.noreply.github.com>
2019-05-05 18:03:02 +0200

    Add files via upload
    new layer and validation dataset are a
    dded

--- project_1/main_with_dropout.py
+++ project_1/main_with_dropout.py
@@ -73,22 +73,28 @@ class Model(nn.Module):
     def __init__(self):
         super(Model, self).__init__()
         self.layer1 = nn.Sequential(
-            nn.Conv2d(2, 16, kernel_size=5, padding=1),
+            nn.Conv2d(2, 16, kernel_size=3, padding=1),
             nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.Dropout(0.2),
             nn.MaxPool2d(2))
         self.layer2 = nn.Sequential(
-            nn.Conv2d(16, 32, kernel_size=5, padding=2),
+            nn.Conv2d(16, 32, kernel_size=2, padding=0),
             nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.Dropout(0.3),
             nn.MaxPool2d(2))
-        self.fc = nn.Linear(288, 2)
+        self.layer3 = nn.Sequential(
+            nn.Conv2d(32, 64, kernel_size=2, padding=0),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Dropout(0.5))
+        self.fc = nn.Linear(256, 2)
 
     def forward(self, x):
         out = self.layer1(x)
         out = self.layer2(out)
+        out = self.layer3(out)
         out = out.view(out.size(0), -1)
         out = self.fc(out)
         return out
@@ -104,18 +110,19 @@ def main():
     """
     # ----- PARAMETER --------------------
     nb_pair = 1000
-    batch_size = 100
+    batch_size = 50
     nb_epochs = 10
     learning_rate = 5e-3
 
     # ----- DATASET --------------------
-    train_input, train_target, train_class, test_input, test_target, test_class = prologue.generate_pair_sets(nb_pair)
+    train_input, train_target, train_class, validation_input, validation_target, validation_class = prologue.generate_pair_sets(nb_pair)
+
 
     train_dataset = TensorDataset(train_input, train_class, train_target)
     train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
 
-    test_dataset = TensorDataset(test_input, test_class, test_target)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
+    validation_dataset = TensorDataset(validation_input, validation_class, validation_target)
+    validation_loader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
     
     # ----- MODEL --------------------
     model = Model()
@@ -129,14 +136,20 @@ def main():
     # ----- TRAINING --------------------
     nb_batch = nb_pair // batch_size
     train(train_loader, model, criterion, optimizer, nb_batch, nb_epochs=nb_epochs)
-
+    _, _, _, test_input, test_target, test_class = prologue.generate_pair_sets(nb_pair)
+    test_dataset = TensorDataset(test_input, test_class, test_target)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True, num_workers=2)
     # ----- EVALUATION --------------------
-    model_test=model.train(False)
-    train_accuracy = compute_accuracy(model_test, train_loader)
-    test_accuracy = compute_accuracy(model_test, test_loader)
+    model_validation=model.train(False)
+    train_accuracy = compute_accuracy(model_validation, train_loader)
+    validation_accuracy = compute_accuracy(model_validation, validation_loader)
+    test_accuracy = compute_accuracy(model_validation, test_loader)
     print('Accuracy on train set: %d %%' % train_accuracy)
+    print('Accuracy on validation set: %d %%' % validation_accuracy)
     print('Accuracy on test set: %d %%' % test_accuracy)
 
 
 if __name__ == '__main__':
     main()
+
+print('end')
